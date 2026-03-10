0a37b55eae08f70c23c6102945d61a16e33d8886
Madue Unity <madue.unity.9@gmail.com>
2026-03-10 16:38:02 +0100

    fx

--- back-end/server.js
+++ back-end/server.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const corsOptions = {
-  origin: "*",
+  origin: "https://phc-reg-wqfa.vercel.app",
   methods: ["GET", "POST", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
